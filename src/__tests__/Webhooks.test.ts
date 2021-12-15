af0dbfa94e30b4a00206cbb173b9a96bc66cd0f1
sebfie <sebastien.fieloux@gmail.com>
2021-12-15 15:08:05 +0100

    Fix jest tests

--- src/__tests__/Webhooks.test.ts
+++ src/__tests__/Webhooks.test.ts
@@ -6,51 +6,47 @@ let nockBack = nock.back;
 nockBack.setMode('record');
 nockBack.fixtures = __dirname + '/fixtures/webhooks';
 
-// test('create webhook cocolis api', async () => {
-//   const CocolisClient = new Cocolis({ live: false });
-
-//   nockBack('create.json', async (nockDone:any) => {
-//     const createWebhooksParams = {
-//       event: "offer_accepted", 
-//       url: "https://www.cocolis.fr/ride_webhook", 
-//       active: false
-//     };
-
-//     await CocolisClient.sign_in({ app_id: 'e0611906', password: 'sebfie' });
-//     var r = await CocolisClient.createWebhook(createWebhooksParams);
-//     nockDone();
-//     expect(r.data.id).toBe(154);
-//   });
-// });
-
-beforeEach(function (done) {
-  if (!nock.isActive()) nock.activate()
-  nock.cleanAll() // clean all pending mocks
-  done()
-})
-  
-it('get webhook cocolis', async () => {
+test('create webhook cocolis api', (resolve) => {
   const CocolisClient = new Cocolis({ live: false });
 
-  nockBack('get.json', async (nockDone:any) => {
-    console.log('webhook get');
+  nockBack('create.json', async (nockDone:any) => {
+    const createWebhooksParams = {
+      event: "offer_accepted",
+      url: "https://www.cocolis.fr/ride_webhook",
+      active: false
+    };
+
+    await CocolisClient.sign_in({ app_id: 'e0611906', password: 'sebfie' });
+    var r = await CocolisClient.createWebhook(createWebhooksParams);
+    nockDone();
+    expect(r.data.id).toBe(157);
+    resolve()
+  });
+});
+
+it('get webhook cocolis', (resolve) => {
+  const CocolisClient = new Cocolis({ live: false });
+
+  nock.back('get.json', async (nockDone:any) => {
     await CocolisClient.sign_in({ app_id: 'e0611906', password: 'sebfie' });
     var r = await CocolisClient.getWebhook(156);
     nockDone();
     expect(r.data.url).toBe("https://www.cocolis.fr/ride_webhook");
+    resolve();
   });
 });
 
 
-it('delete webhook cocolis api', async () => {
+it('delete webhook cocolis api', (resolve) => {
   const CocolisClient = new Cocolis({ live: false });
 
-  nockBack('delete.json', async (nockDone:any) => {
-    console.log('webhook delete');
+  nock.back('delete.json', async (nockDone: any) => {
     var auth = await CocolisClient.sign_in({ app_id: 'e0611906', password: 'sebfie' });
     var r = await CocolisClient.deleteWebhook(156);
     nockDone();
     expect(r.status).toBe(204);
+    resolve();
   });
+  console.log('end delete webhook cocolis api');
 });
 
